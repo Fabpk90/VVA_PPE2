00664995c45ba18bfc0ed26af621dc27e5b6ccc0
Fabpk90 <fabrizio1996@live.fr>
2016-12-15 10:03:11 +0100

    starting welcome page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Site communautaire</title>
+    <link href="script/css/bootstrap.css" rel="stylesheet">
+    <script src="script/js/jquery.min.js"></script>
+    <script src="script/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+    <h1>Test </h1>
+
+  </body>
+</html>
